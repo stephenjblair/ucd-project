fb98ee34a180515fbb09a1df1803ab32c2c374b2
Blair <stephenjohnblair@outlook.com>
2019-02-15 10:29:35 +0000

    Made navbar responsive

---
+++ assets/js/scripts.js
@@ -0,0 +1,8 @@
+//Responsive Navbar Toggle
+
+        let mainNav = document.getElementById('js-menu');
+        let navBarToggle = document.getElementById('js-navbar-toggle');
+        navBarToggle.addEventListener('click', function () {
+        mainNav.classList.toggle('active');
+        });
+
